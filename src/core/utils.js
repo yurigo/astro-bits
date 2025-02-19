fb26c03dab1b1f54ae6220a2a0f8b2b538cc445a
Yuri <ygonzoo@gmail.com>
2025-02-19 19:20:00 +0100

    add ogl gallery

---
+++ src/core/utils.js
@@ -0,0 +1,28 @@
+export const debounce = (func, wait, immediate) => {
+  let timeout;
+  return function () {
+    const context = this,
+      args = arguments;
+    const later = () => {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+export const lerp = (start, end, t) => {
+  return start * (1 - t) + end * t;
+};
+
+export function autoBind(obj) {
+  for (let key of Object.getOwnPropertyNames(obj.constructor.prototype)) {
+    const val = obj[key];
+    if (key !== "constructor" && typeof val === "function") {
+      obj[key] = val.bind(obj);
+    }
+  }
+}
